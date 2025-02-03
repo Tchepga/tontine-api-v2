45b99eadcf61195c859d29db2d4e7973bdf5c44c
Tchepga <azera9730@gmail.com>
2025-02-03 08:21:02 +0100

    update notification message

--- src/notification/utility/message-notification.ts
+++ src/notification/utility/message-notification.ts
@@ -3,30 +3,30 @@ import { CreateNotificationDto } from '../dto/create-notification.dto';
 import { Loan } from 'src/loan/entities/loan.entity';
 
 export const messageNotificationCreate = {
-    event: "L'évènement a été créé",
-    tontine: 'La tontine a été créée',
-    member: 'Le membre a été créé',
-    sanction: 'La sanction a été créée',
-    deposit: 'Le dépôt a été créé',
-    loan: 'Le prêt a été créé',
+    event: "Un évènement a été créé",
+    tontine: 'Une tontine a été créée',
+    member: 'Un membre a été créé',
+    sanction: 'Une sanction a été créée',
+    deposit: 'Un dépôt a été créé',
+    loan: 'Un prêt a été créé',
 };
 
 export const messageNotificationUpdate = {
-    event: "L'évènement a été modifié",
-    tontine: 'La tontine a été modifiée',
-    member: 'Le membre a été modifié',
-    sanction: 'La sanction a été modifiée',
-    deposit: 'Le dépôt a été modifié',
-    loan: 'Le prêt a été modifié',
+    event: "Un évènement a été modifié",
+    tontine: 'Une tontine a été modifiée',
+    member: 'Un membre a été modifié',
+    sanction: 'Une sanction a été modifiée',
+    deposit: 'Un dépôt a été modifié',
+    loan: 'Un prêt a été modifié',
 };
 
 export const messageNotificationDelete = {
-    event: "L'évènement a été supprimé",
-    tontine: 'La tontine a été supprimée',
-    member: 'Le membre a été supprimé',
-    sanction: 'La sanction a été supprimée',
-    deposit: 'Le dépôt a été supprimé',
-    loan: 'Le prêt a été supprimé',
+    event: "Un évènement a été supprimé",
+    tontine: 'Une tontine a été supprimée',
+    member: 'Un membre a été supprimé',
+    sanction: 'Une sanction a été supprimée',
+    deposit: 'Un dépôt a été supprimé',
+    loan: 'Un prêt a été supprimé',
 };
 
 export enum Action {
