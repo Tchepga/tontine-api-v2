0a7521a4536b9bf9d3994277f6ce238d5f8fae91
Patrick Tchepga <tchepgapatrick@yahoo.fr>
2026-05-31 19:04:44 +0200

    chore: update environment configuration for development and modify server startup to use environment variables

--- src/main.ts
+++ src/main.ts
@@ -18,9 +18,11 @@ async function bootstrap() {
   // TODO: need to restrict validation
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
-  await app.listen({
-    port: 8080,
-    host: 'localhost',
-  });
+
+  const port = parseInt(process.env.PORT || '3000', 10);
+  const host = process.env.HOST || '127.0.0.1';
+
+  await app.listen({ port, host });
+  console.log(`Application running on http://${host}:${port}/api`);
 }
 bootstrap();
