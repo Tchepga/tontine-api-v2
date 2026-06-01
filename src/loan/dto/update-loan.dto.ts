999aebee997f66227002b1394d7239092bd7867f
Patrick Tchepga <tchepgapatrick@yahoo.fr>
2026-06-01 22:31:37 +0200

    feat: updatl dto loan

--- src/loan/dto/update-loan.dto.ts
+++ src/loan/dto/update-loan.dto.ts
@@ -1,18 +1,21 @@
-import { IsEnum, IsNumber, Min } from 'class-validator';
+import { IsEnum, IsNumber, IsOptional, Min } from 'class-validator';
 import { Member } from 'src/member/entities/member.entity';
 import { StatusLoan } from '../enum/status-loan';
 
 export class UpdateLoanDto {
+  @IsOptional()
   @IsNumber({}, { message: 'Amount must be a number' })
   @Min(1, { message: 'Amount must be at least 1' })
   amount?: number;
 
+  @IsOptional()
   @IsEnum(StatusLoan, {
     message:
       'Status must be PENDING, APPROVED, PAID, CANCELLED or REJECTED',
   })
   status?: StatusLoan;
 
+  @IsOptional()
   @IsEnum(['EUR', 'FCFA', 'USD'], {
     message: 'Currency must be one of the following: euro, fcfa, dollar',
   })
